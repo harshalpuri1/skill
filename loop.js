c5dc42ffc00135d6035652a5558f3cefc93eb4a3
Harshal <hrp3102002@gmail.com>
2023-10-08 20:40:54 +0530

    added Loop

---
+++ loop.js
@@ -0,0 +1,37 @@
+// print Hello World! 100 times
+for (let count=1; count<=100; count++)
+{
+    console.log("Hello World!");
+
+}          
+console.log('-------------------------------');   
+
+
+// print 1 to 10
+for (let i=1; i<=10; i++){
+    console.log(i);  
+}
+console.log('-------------------------------');
+
+
+// odd numbers
+for (let i=1; i<=10; i=i+2)
+{
+    console.log(i);  
+}
+console.log('-------------------------------');
+
+// even numbers
+for (let j = 2 ;j <=50; j+=2){
+    console.log(j);
+}
+console.log('-------------------------------');
+
+
+// print 10 to 1
+
+for (let a=10; a>=1; a--)
+{
+    console.log(a);
+}
+console.log('-------------------------------');
