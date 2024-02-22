9d6b4b30207c4e6d9823e505683a88b190d60297
Harshal Puri <hrp3102002@gmail.com>
2024-02-22 13:13:56 +0530

    Learn Flex & Grid

--- JAVASCRIPT/loop.js
+++ JAVASCRIPT/loop.js
@@ -208,15 +208,6 @@ star (pattern);
   
 
    
-    // for(let i=1; i<=10; i++){
-    //     let stars = "";
-    //     for (let j=1; j<=i; j++){
-    //         stars += "*";
-
-    //     }
-    //     console.log(stars);
-
-    // }
 
 
 
