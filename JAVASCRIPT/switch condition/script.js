7d74e43af24a3c6f5c3a0eb528b64e95dc2f7a99
Harshal Puri <hrp3102002@gmail.com>
2024-02-27 14:37:51 +0530

    Added some Notes

---
+++ JAVASCRIPT/switch condition/script.js
@@ -0,0 +1,54 @@
+
+// let n = 9;
+
+// switch (n){
+//     case 1:
+//         console.log("Sunday");
+//         break;
+
+//     case 2:
+//         console.log("Monday");
+//         break;
+
+//     case 3:
+//         console.log("Tuesday");
+//         break;
+    
+//     case 4:
+//         console.log("Wednesday");
+//         break;
+    
+//     case 5:
+//         console.log("Thusday");
+//         break;
+    
+//     case 6:
+//         console.log("Friday");
+//         break;
+
+//     case 7:
+//         console.log("Saturday");
+//         break;
+
+//         default :
+//         console.log("Invalid Day Number")
+
+// }
+
+
+// var x=15;
+// var y = 5;
+// var z = 3;
+
+// if (x==y){
+//     document.write(y);
+// }
+// else if (x>=y){
+//     document.write(x);
+// }
+// else{
+//     document.write(z);
+// }
+
+let msg = 'jflkjd';
+console.log((msg.toUpperCase()));
